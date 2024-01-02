84cf4a6d163de9e43f8acb3ead57dafee83c88bc
Ashutosh Mishra <ashutosh.mishra@tothenew.com>
2024-01-02 21:08:29 +0530

    Initial Commit

---
+++ Dinosour/DinosourApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinosourApp.swift
+//  Dinosour
+//
+//  Created by TTNPL-3940 on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DinosourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DinosourUITests/DinosourUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DinosourUITestsLaunchTests.swift
+//  DinosourUITests
+//
+//  Created by TTNPL-3940 on 02/01/24.
+//
+
+import XCTest
+
+final class DinosourUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
